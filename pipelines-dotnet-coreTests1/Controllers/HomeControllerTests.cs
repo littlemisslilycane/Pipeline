e5c989ad78966296a49784c1a64092c6959e2daf
littlemisslilycane <Vasavi.Venkatesh@philips.com>
2020-08-12 14:59:37 -0400

    .

---
+++ pipelines-dotnet-coreTests1/Controllers/HomeControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using pipelines_dotnet_core.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pipelines_dotnet_core.Controllers.Tests
+{
+	[TestClass()]
+	public class HomeControllerTests
+	{
+		[TestMethod()]
+		public void IndexTest()
+		{
+			Assert.Fail();
+		}
+	}
+}
